dea97a826f164102e9a0aafec6b439c1f90b8096
Merritt Dombos <107428733+mldombos@users.noreply.github.com>
2022-07-13 15:11:17 -0400

    changes

--- src/components/qrCode.js
+++ src/components/qrCode.js
@@ -2,11 +2,11 @@ import QRCode from "react-qr-code";
 import { withLDConsumer } from "launchdarkly-react-client-sdk";
 
 //Change QRURL to the URL where you'll be hosting this app
-const QRURL = "https://ttotenberg-ld.github.io/react_qr_app/";
+const QRURL = "http://mldombos.github.io/react_qr_app/";
 
 const qrCodeHome = ({ flags, ldClient /*, ...otherProps */ }) => {
   let showFeature = ldClient.variation("reactQRCode");
-  
+
   return showFeature ? (
     <div>
       <br />
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 import { deviceType, osName } from "react-device-detect";
 import getUserId from "./util/getUserId";
 
-const CLIENTKEY = "609ead905193530d7c28647b";
+const CLIENTKEY = "62cf0e00d9da4210d8a98002";
 
 let id = getUserId();
 
