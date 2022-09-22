873d7c0b54c6f44be9fe759bf933f836e1254d4f
Kevin Cochran <kcochran@launchdarkly.com>
2022-09-22 17:34:09 -0400

    update with kevincloud info

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 import { deviceType, osName } from "react-device-detect";
 import getUserId from "./util/getUserId";
 
-const CLIENTKEY = "609ead905193530d7c28647b";
+const CLIENTKEY = "632a4124f983701215cfd2a2";
 
 let id = getUserId();
 
