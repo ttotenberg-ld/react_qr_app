ff2b6cf7d7ef2bbe46ae327a0e6176d6cddd324e
Yoz Grahame <173848+yozlet@users.noreply.github.com>
2023-02-16 10:50:45 -0800

    Revert "Get Clientkey from url, add form"

--- src/components/qrCode.js
+++ src/components/qrCode.js
@@ -2,7 +2,7 @@ import QRCode from "react-qr-code";
 import { withLDConsumer } from "launchdarkly-react-client-sdk";
 
 //Change QRURL to the URL where you'll be hosting this app
-const QRURL = "https://launchdarkly-labs.github.io/react_qr_app_ld_beginners/";
+const QRURL = "https://ttotenberg-ld.github.io/react_qr_app/";
 
 const qrCodeHome = ({ flags }) => {
 
