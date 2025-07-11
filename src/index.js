23187be276546402a4e3282a10f8fdd8d035558a
Tom Totenberg <ttotenberg@launchdarkly.com>
2025-07-11 09:05:56 -0500

    Fix React rendering issue: update index.js to use correct DOM element and modern React imports

--- src/index.js
+++ src/index.js
@@ -1,38 +1,40 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 import { deviceType, osName } from "react-device-detect";
 import getUserId from "./util/getUserId";
 
 const CLIENTKEY = "609ead905193530d7c28647b";
 
-let id = getUserId();
+let user_id = getUserId();
+
+
+const ldContext = {
+  kind: 'multi',
+  user: {
+    key: user_id,
+    name: 'Anna',
+    role: 'doctor'
+  },
+  device: {
+    key: 'device-key-123abc',
+    os: osName,
+    deviceType: deviceType,
+  }
+};
 
 (async () => {
   const LDProvider = await asyncWithLDProvider({
     clientSideID: CLIENTKEY,
-    user: {
-      key: id,
-      //dynamically set these custom attributes using the deviceType and osName selectors from the npm package
-      custom: {
-        device: deviceType,
-        operatingSystem: osName,
-      },
-    },
+    context: ldContext,
   });
 
-  ReactDOM.render(
+  render(
     <LDProvider>
       <App />
     </LDProvider>,
-    document.getElementById("root")
+    document.getElementById('root'),
   );
 })();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
